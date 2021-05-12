8746162d77b193ac141546a7be368003597b02ac
Anatoliy Melnikau <amelnikov@aristeksystems.com>
2021-05-12 22:55:59 +0300

    Extract finder of missed values

---
+++ src/Service/Yaml/MissedValuesFinder.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Aeliot\Bundle\TransMaintain\Service\Yaml;
+
+final class MissedValuesFinder
+{
+    private FilesFinder $filesFinder;
+    private KeysParser $keysParser;
+
+    public function __construct(FilesFinder $filesFinder, KeysParser $keysParser)
+    {
+        $this->filesFinder = $filesFinder;
+        $this->keysParser = $keysParser;
+    }
+
+    public function findMissedTranslations(string $domain, string $sourceLocale, ?string $targetLocale): array
+    {
+        $domainsFiles = $this->filesFinder->getFilesMap();
+
+        if (!isset($domainsFiles[$domain][$sourceLocale])) {
+            throw new \InvalidArgumentException(
+                \sprintf('Invalid domain "%s" or locale "%s" posted', $domain, $sourceLocale)
+            );
+        }
+
+        $parsedKeys = $this->keysParser->getParsedKeys($domainsFiles[$domain]);
+        $omittedKeys = $this->keysParser->getOmittedKeys($parsedKeys);
+        $allOmittedKeys = $this->keysParser->mergeKeys($omittedKeys);
+
+        $values = array_intersect_key(
+            $this->keysParser->parseFiles($domainsFiles[$domain][$sourceLocale]),
+            array_flip($allOmittedKeys)
+        );
+
+        if ($targetLocale) {
+            if (!$filterKeys = $omittedKeys[$targetLocale] ?? null) {
+                throw new \InvalidArgumentException(\sprintf('There is no omitted keys for locale "%s"', $targetLocale));
+            }
+
+            $values = array_intersect_key($values, array_flip($filterKeys));
+        }
+
+        ksort($values);
+
+        return $values;
+    }
+}
