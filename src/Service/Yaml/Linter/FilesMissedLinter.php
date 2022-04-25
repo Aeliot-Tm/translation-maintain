c1f33e28676ec9e97e06c4d97a22a7806f607d3b
Anatoliy Melnikau <amelnikov@aristeksystems.com>
2022-04-25 21:43:23 +0300

    Apply standard files filter

--- src/Service/Yaml/Linter/FilesMissedLinter.php
+++ src/Service/Yaml/Linter/FilesMissedLinter.php
@@ -7,16 +7,18 @@ namespace Aeliot\Bundle\TransMaintain\Service\Yaml\Linter;
 use Aeliot\Bundle\TransMaintain\Dto\LintYamlFilterDto;
 use Aeliot\Bundle\TransMaintain\Model\FilesMissedLine;
 use Aeliot\Bundle\TransMaintain\Model\ReportBag;
+use Aeliot\Bundle\TransMaintain\Service\Yaml\FileMapFilter;
 use Aeliot\Bundle\TransMaintain\Service\Yaml\FilesFinder;
-use Aeliot\Bundle\TransMaintain\Service\Yaml\LinterRegistry;
 
 final class FilesMissedLinter implements LinterInterface
 {
     private FilesFinder $filesFinder;
+    private FileMapFilter $fileMapFilter;
 
-    public function __construct(FilesFinder $filesFinder)
+    public function __construct(FilesFinder $filesFinder, FileMapFilter $fileMapFilter)
     {
         $this->filesFinder = $filesFinder;
+        $this->fileMapFilter = $fileMapFilter;
     }
 
     public function getKey(): string
@@ -32,18 +34,11 @@ final class FilesMissedLinter implements LinterInterface
     public function lint(LintYamlFilterDto $filterDto): ReportBag
     {
         $bag = new ReportBag(FilesMissedLine::class);
-        $filesMap = $this->filesFinder->getFilesMap();
+        $domainsFiles = $this->fileMapFilter->getFilesMap($filterDto);
         $mentionedLocales = $this->filesFinder->getLocales();
-        foreach ($filesMap as $domain => $localesFiles) {
-            if ($filterDto->domains && !\in_array($domain, $filterDto->domains, true)) {
-                continue;
-            }
-            $omitted = array_diff($mentionedLocales, array_keys($localesFiles));
-            if ($filterDto->locales) {
-                $omitted = \array_intersect($omitted, $filterDto->locales);
-            }
-            if ($omitted) {
-                $bag->addLine(new FilesMissedLine($domain, $omitted));
+        foreach ($domainsFiles as $domain => $localesFiles) {
+            if ($omittedLocales = array_diff($mentionedLocales, array_keys($localesFiles))) {
+                $bag->addLine(new FilesMissedLine($domain, $omittedLocales));
             }
         }
 
