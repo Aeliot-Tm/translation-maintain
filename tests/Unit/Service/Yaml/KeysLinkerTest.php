ce259e047584191eaf1a5d288bbc4fee622eec8b
Anatoliy Melnikau <amelnikov@aristeksystems.com>
2022-05-02 09:29:43 +0300

    Implement test for Yaml\KeysLinker

---
+++ tests/Unit/Service/Yaml/KeysLinkerTest.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Aeliot\Bundle\TransMaintain\Test\Unit\Service\Yaml;
+
+use Aeliot\Bundle\TransMaintain\Service\Yaml\KeysLinker;
+use Generator;
+use PHPUnit\Framework\TestCase;
+
+final class KeysLinkerTest extends TestCase
+{
+    /**
+     * @dataProvider getDataForTestGlueKeys
+     *
+     * @param array<string,string> $expected
+     * @param array<string,mixed> $income
+     */
+    public function testGlueKeys(array $expected, array $income): void
+    {
+        self::assertSame($expected, iterator_to_array((new KeysLinker())->glueKeys($income)));
+    }
+
+    /**
+     * @return Generator<array<array<string,mixed>>
+     */
+    public function getDataForTestGlueKeys(): \Generator
+    {
+        yield [
+            ['a.b' => '*', 'a.c' => '*', 'a.d.e' => '*'],
+            ['a' => ['b' => '*', 'c' => '*', 'd' => ['e' => '*']]],
+        ];
+    }
+}
